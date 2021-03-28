9571c009bca88851a55c30151f78644d65e4b6bf
Cavendishh <kavanderjanne@gmail.com>
2021-03-28 21:31:11 +0300

    Testing and learning different Ion components

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,74 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import React from 'react'
+import './Tab2.css'
+import { IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonGrid,
+  IonText,
+  IonRow,
+  IonCol
+} from '@ionic/react'
+
+const testfnc = () => {
+  console.log('Test function working')
+}
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
+
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Reserve - sandbox</IonTitle>
+          <IonTitle>Reserve</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Reserve</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Reserve page" />
-      </IonContent>
-    </IonPage>
-  );
-};
 
-export default Tab2;
+
+      <IonContent>
+        <IonGrid>
+          <IonRow>
+            <IonCol>
+              <IonButton color='secondary'>Laundry</IonButton>
+            </IonCol>
+            <IonCol>
+              <IonButton color='secondary' >Sauna</IonButton>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonText>Your Reservations</IonText>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonText>Sun 14/03/2021 @ 10-11 - Washer 1</IonText>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+              <IonText>Sun 14/03/2021 @ 10-11 - Washer 2</IonText>
+            </IonCol>
+          </IonRow>
+
+
+          <IonRow>
+            <IonCol>
+            <IonButton color='secondary' onClick={testfnc}>Test</IonButton>
+            </IonCol>
+          </IonRow>
+
+
+        </IonGrid>
+      </IonContent>
+      
+    </IonPage>
+  )
+}
+
+export default Tab2
