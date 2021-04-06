8b15b420755be52dda98f44662facac95fcc5e05
ssirina <54996635+ssirina@users.noreply.github.com>
2021-04-06 12:59:48 +0300

    Adding structure

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,8 +1,12 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
 import './Tab4.css';
 
 const Tab4: React.FC = () => {
+  const [showModal, setShowModal] = useState(false);
+
+   /*--   Tähän pitäisi tulla ne consteina eri toiminnot, kuten addItem. Tee uuden ilmoituksen lisääminen Modalin avulla --*/
   return (
     <IonPage>
       <IonHeader>
@@ -11,13 +15,53 @@ const Tab4: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Fault Report</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Fault Report page" />
+
+      <IonButton onClick={() => setShowModal(true)}>
+            Add new
+      </IonButton>
+
+        <IonModal isOpen={showModal}>
+          <IonPage>
+            <IonHeader>
+              <IonToolbar>
+                <IonTitle>New fault report</IonTitle>
+              </IonToolbar>
+            </IonHeader>
+            <p>This is the modal content.</p>
+            <IonButton onClick={() => setShowModal(false)}>
+              Send
+            </IonButton>
+          </IonPage>
+      </IonModal>
+
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>
+              Leaking waterpipe
+            </IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            Tässä tulee olemaan kortteina tehdyt ilmoitukset.
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle>
+              Jotain
+            </IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            Jotain sisältöä tässäkin kortissa
+          </IonCardContent>
+        </IonCard> 
+        <div>
+        <h3>Emergency contact details</h3>
+        <p>In case of emergency for ex. severely leaking water pipe or dangerous elecrical fault.</p>
+        <p>24/7</p>
+        <p>+358 123 456 789</p>
+      </div>
       </IonContent>
+
     </IonPage>
   );
 };
