3278cdb312ab2d3503e5bd2c6c7ac030695cad70
VerjaSol <70750728+VerjaSol@users.noreply.github.com>
2021-04-14 18:01:12 +0300

    muutoksia14.4

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,18 +1,76 @@
-import { IonContent, IonHeader, IonPage, IonIcon, IonCardHeader, IonCardTitle, IonTitle, IonToolbar, IonPopover, IonButton, IonCard, IonCardContent,  IonTextarea, IonItem, IonLabel, IonItemDivider} from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import React, { useState } from 'react';
-import { personOutline } from 'ionicons/icons';
-import './Tab3.css';
-
+import {
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonPage,
+  IonIcon,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonTitle,
+  IonToolbar,
+  IonPopover,
+  IonButton,
+  IonCard,
+  IonList,
+  IonCardContent,
+  IonTextarea,
+  IonItem,
+  IonLabel,
+  IonItemDivider,
+  IonText
   
+} from "@ionic/react";
+
+import React, { useState } from "react";
+import Collapsible from "react-collapsible";
+import { personOutline } from "ionicons/icons";
+import "./Tab3.css";
+
+export interface Item {
+  id: number;
+  name: string;
+  title: string;
+  text: string;
+}
+
 const Tab3: React.FC = () => {
-  const [popoverState, setShowPopover] = useState({ showPopover: false, event: undefined });
-  const [text, setText] = useState<string>();
+  const [popoverState, setShowPopover] = useState({
+    showPopover: false,
+    event: undefined,
+  });
+
+
+  const [text, setText] = useState('');
+  const [name, setName] = useState('');
+  const [title, setTitle]= useState('')
+  const [items, setItems] = useState<Item[]>([]);
+  const [state, setState]=useState({
+    })
+
+    const addItem = () => {
+      const nextId = Math.random()
+      const item: Item = {
+        id: nextId,
+        name,
+        title,
+        text
+      };
+      setItems([item,...items]);
+      setName('');
+      setTitle('');
+      setText('');
+    };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Meeting Room</IonTitle>
+        <IonText color="primary">
+          <h2>
+        <IonTitle class="title"><img alt="building" src="../assets/home.jpg" />HOME CHANNEL</IonTitle>
+        </h2>
+        </IonText>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -21,50 +79,135 @@ const Tab3: React.FC = () => {
             <IonTitle size="large">Meeting Room</IonTitle>
           </IonToolbar>
         </IonHeader>
+        
         <IonPopover
-        cssClass='my-custom-class'
-        event={popoverState.event}
-        isOpen={popoverState.showPopover}
-        onDidDismiss={() => setShowPopover({ showPopover: false, event: undefined })}
-      >
-        <IonItemDivider>Your message</IonItemDivider>          
+          cssClass="my-custom-class"
+          event={popoverState.event}
+          isOpen={popoverState.showPopover}
+          onDidDismiss={() =>
+            setShowPopover({ showPopover: false, event: undefined })
+          }
+        >
+          
+          <IonItemDivider>Your message</IonItemDivider>
+          
           <IonItem>
-            <IonTextarea value={text} onIonChange={e => setText(e.detail.value!)}></IonTextarea>
+            <IonLabel position={"floating"}>Your name</IonLabel>
+            <IonInput value={name} onIonChange={e => setName(e.detail.value!)} ></IonInput>
           </IonItem>
-          <IonButton>Send</IonButton>
-      </IonPopover>
-      <IonButton onClick={
-        (e: any) => {
-          e.persist();
-          setShowPopover({ showPopover: true, event: e })
-        }}
-      >
-        Add new
-      </IonButton>
-      <IonCard>
-        <IonIcon icon={personOutline}></IonIcon>
-        <IonCardHeader>
-            <IonCardTitle>Mike Simons</IonCardTitle>
-          </IonCardHeader>
-        <IonCardContent>
-            A table for sale
-        </IonCardContent>
-      </IonCard>
-      <IonCard>
-        <IonItem className="ion-activated">
-        <IonIcon icon={personOutline}></IonIcon>
-        <IonCardHeader>
-            <IonCardTitle>Mike Simons</IonCardTitle>
-          </IonCardHeader>
-        <IonCardContent>
-            A table for sale
-        </IonCardContent>
+          <IonItem>
+            <IonLabel position={"floating"}>Title</IonLabel>
+            <IonInput value={title} onIonChange={e => setTitle(e.detail.value!)}  ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonLabel position={"floating"}>Add your text</IonLabel>
+            <IonTextarea value={text} onIonChange={e => setText(e.detail.value!)} ></IonTextarea>
+          </IonItem>
+          <IonButton expand="block" 
+          onClick={(e: any) => {
+            addItem();
+            setShowPopover({ showPopover: false, event: e });
+          }}
+           >Send</IonButton> 
+
+        </IonPopover>
+        <IonButton
+          expand="block"
+          onClick={(e: any) => {
+            e.persist();
+            setShowPopover({ showPopover: true, event: e });
+          }}
+        >
+          Add new
+        </IonButton>
+
+        <IonList>
             
-        </IonItem>
-      </IonCard>
+              {items.map((item, i) => (
+                
+                   <IonCard class="card" key={i}>
+                     
+                    <IonCardHeader>
+                  <IonCardTitle>
+                    <IonIcon class="icon" icon={personOutline}></IonIcon>{item.name}
+                    </IonCardTitle>
+                    <IonCardSubtitle> 
+                    {item.title}
+                  </IonCardSubtitle>
+                  
+                  </IonCardHeader>
+          <Collapsible id="a" trigger="Read more...">
+            <IonCardContent>
+              {item.text}
+            </IonCardContent>
+          </Collapsible>
+             
+        </IonCard>
+              ))}
+            </IonList>
+
+        <IonCard class="card">
+          <IonCardHeader>
+            <IonCardTitle>
+              <IonIcon class="icon" icon={personOutline}></IonIcon> Mike Simons
+            </IonCardTitle>
+            <IonCardSubtitle>A table for sale</IonCardSubtitle>
+          </IonCardHeader>
+          <Collapsible id="a" trigger="Read more...">
+            <IonCardContent>
+              A table for sale. Almost new table 80x120. 10 eur. Ask more by
+              message 04000040404
+            </IonCardContent>
+          </Collapsible>
+        </IonCard>
+
+        <IonCard class="card">
+          <IonCardHeader>
+            <IonCardTitle>
+              <IonIcon class="icon" icon={personOutline}></IonIcon> Mike Simons
+            </IonCardTitle>
+            <IonCardSubtitle>A table for sale</IonCardSubtitle>
+          </IonCardHeader>
+          <Collapsible id="a" trigger="Read more...">
+            <IonCardContent>
+              A table for sale. Almost new table 80x120. 10 eur. Ask more by
+              message 04000040404
+            </IonCardContent>
+          </Collapsible>
+        </IonCard>
+        <IonCard class="card">
+          <IonCardHeader>
+            <IonCardTitle>
+              <IonIcon class="icon" icon={personOutline}></IonIcon> Mike Simons
+            </IonCardTitle>
+            <IonCardSubtitle>A table for sale</IonCardSubtitle>
+          </IonCardHeader>
+          <Collapsible id="a" trigger="Read more...">
+            <IonCardContent>
+              A table for sale. Almost new table 80x120. 10 eur. Ask more by
+              message 04000040404
+            </IonCardContent>
+          </Collapsible>
+        </IonCard>
+        <IonCard class="card">
+          <IonCardHeader>
+            <IonCardTitle>
+              <IonIcon class="icon" icon={personOutline}></IonIcon> Mike Simons
+            </IonCardTitle>
+            <IonCardSubtitle>A table for sale</IonCardSubtitle>
+          </IonCardHeader>
+          <Collapsible id="a" trigger="Read more...">
+            <IonCardContent>
+              A table for sale. Almost new table 80x120. 10 eur. Ask more by
+              message 04000040404
+            </IonCardContent>
+          </Collapsible>
+        </IonCard>
+        
+        
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab3;
+export default Tab3
--- src/pages/Tab5.tsx
+++ src/pages/Tab5.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCardContent, IonCard, IonIcon, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { callOutline } from 'ionicons/icons';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab5.css';
 
@@ -16,7 +17,32 @@ const Tab5: React.FC = () => {
             <IonTitle size="large">Property Info</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Property Info page" />
+        <IonCard>
+          
+          <IonCardTitle><p>Property Info</p></IonCardTitle>
+          <IonCardContent >
+           <p> Fleminginkatu 21</p>
+            <p>Floors: 7</p>
+            <p>Apartments: 256</p>
+            <p>Sauna: B (7th floor)</p>
+            <p>Laundry: B (1st Floors)</p>
+
+            <p>Property maintenance service</p>
+            <p>ABC Ltd (9.00 - 16.00)</p>
+            <p>+358 98764532</p>
+           <p><a href="Google.com">www.service-abc fi</a></p> 
+           <p> Door opening:</p>
+           <p>+358 987654321</p>
+          </IonCardContent>
+        </IonCard>
+        <IonCard>
+          <IonCardTitle><p>Emergency contact details</p></IonCardTitle>
+          <IonCardContent >
+           <p><h2>24/7</h2> </p>
+            
+            <p><IonIcon class="icon" icon={callOutline}></IonIcon> +358 987654321</p>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
