02bca4dc629cec8b683ef635769301843f963e5d
ssirina <54996635+ssirina@users.noreply.github.com>
2021-04-20 23:28:40 +0300

    cards, camera funtion, CSS, etc.

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,7 +1,35 @@
-import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonModal, IonPage, IonTextarea, IonTitle, IonToolbar, IonToggle, IonGrid, IonRow } from '@ionic/react';
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonPage,
+  IonTextarea,
+  IonTitle,
+  IonToolbar,
+  IonToggle,
+  IonGrid,
+  IonRow,
+  IonImg,
+  IonCol,
+  IonFooter
+} from '@ionic/react';
+
 import React, { useState } from 'react';
 import './Tab4.css';
-import { attachOutline, cameraOutline, micOutline } from 'ionicons/icons';
+import { cameraOutline, image, micOutline, chevronDownCircleOutline, chevronUpCircleOutline} from 'ionicons/icons';
+import Collapsible from 'react-collapsible';
+import { Plugins, CameraResultType, Camera } from "@capacitor/core";
+
 
 
 export interface Report {
@@ -10,6 +38,7 @@ export interface Report {
   content: string;
   masterkey: boolean;
   call: boolean
+  image: any
 }
 
 function masterToggle(toggle) {
@@ -28,6 +57,21 @@ function callToggle(toggle) {
   }
 }
 
+async function takePicture()  {
+  try {
+    const picture = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Base64,
+    });
+    this.guestPicture = picture.base64String;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+
 const Tab4: React.FC = () => {
   const [showModal, setShowModal] = useState(false);
   const [showElement, setShowElement] = useState(true);
@@ -37,6 +81,23 @@ const Tab4: React.FC = () => {
   const [call, setCall] = useState(false)
   const [reports, setReports] = useState<Report[]>([]);
   const newReports = [...reports].reverse();
+  const newReportsTwo=[...newReports].filter(e => e.text !== "" && e.content !== "");
+  const { Camera } = Plugins;
+
+  
+  const Trigger = () => (
+    <IonItem>
+      <IonIcon icon={chevronDownCircleOutline} slot="end"/>
+    </IonItem>
+  );
+
+  const Trigger2 = () => (
+    <IonItem>
+      <IonIcon icon={chevronUpCircleOutline} slot="end"/>
+    </IonItem>
+  )
+  ;
+
 
   const addReport = () => {
     const nextID = Math.random()
@@ -45,7 +106,8 @@ const Tab4: React.FC = () => {
       text: text,
       content: content,
       masterkey: masterkey,
-      call: call
+      call: call,
+      image: image
     };
     setReports([...reports, item]);
     setShowModal(false);
@@ -55,9 +117,6 @@ const Tab4: React.FC = () => {
     setCall(false);
   }
 
- 
- 
-
   return (
     <IonPage>
       <IonHeader>
@@ -65,40 +124,41 @@ const Tab4: React.FC = () => {
           <IonTitle>Fault Report</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent className="ion-padding">
 
         <IonGrid>
-          <IonRow>
-            <IonButton onClick={() => setShowModal(true)}>Add new</IonButton>
+          <IonRow className="ion-justify-content-center">
+            <IonCol>
+             <IonButton color="secondary" expand="block" onClick={() => setShowModal(true)}>Add new</IonButton>
+            </IonCol>
           </IonRow>
           <IonRow>
-          {newReports.length > 0 && (
-            <IonList>
-              {newReports.map((list, i) => (
-                  <IonCard key={i}>
-                    <IonCardTitle>
-                      <h2>{list.text}</h2>
-                    </IonCardTitle>
-                    <IonCardContent>
-                      <p>{list.content}</p>
-                      <p>{masterToggle(list.masterkey)}</p>
-                      <p>{callToggle(list.call)}</p>
-                    </IonCardContent>
-                  </IonCard>
-              ))}
-            </IonList>
-          )}
-          </IonRow>
-          <IonRow>
-            <div>
-              <h3>Emergency contact details</h3>
-              <p>In case of emergency for ex. severely leaking water pipe or dangerous elecrical fault.</p>
-              <p>24/7</p>
-              <p>+358 123 456 789</p>
-            </div>
-          </IonRow>
-          <IonRow>
-     
+            <IonCol>
+              {newReportsTwo.length > 0 && (
+              <IonList lines="none">
+                {newReportsTwo.map((list, i) => (
+                    <IonCard className="card" key={i}>
+                      <IonCardHeader>
+                        <h2>{list.text}</h2>
+                      </IonCardHeader>
+                      <Collapsible trigger={<Trigger />}>
+
+                      <IonCardContent>
+                        <p id="report">{list.content}</p>
+                        <br/>
+                        <p id="reportToggle">{masterToggle(list.masterkey)}</p>
+                        <p id="reportToggle">{callToggle(list.call)}</p>
+                        {/*<IonImg src={list.image} />*/}
+                        <IonImg src= "https://images.unsplash.com/photo-1600247250062-7bc3adb28177?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1270&q=80"/>
+                        <p>An example image</p>
+                      </IonCardContent>
+                      </Collapsible>
+                    </IonCard>
+                  ))}
+                </IonList>
+              )}
+            </IonCol>
+          
           </IonRow>
         </IonGrid>
 
@@ -121,40 +181,64 @@ const Tab4: React.FC = () => {
                 </IonInput>
             </IonRow>
             <IonRow>
-            <IonButton onClick={() => setShowElement(true)}>Write message</IonButton>
-            <IonButton onClick={() => setShowElement(false)}>Record message</IonButton>
+              <IonCol>
+                <IonButton className="writeRecord" color={showElement ? "tertiary" : "secondary"} onClick={() => setShowElement(true)}>Write</IonButton>
+              </IonCol>
+              <IonCol>
+                <IonButton className="writeRecord" color={showElement ? "secondary" : "tertiary"} onClick={() => setShowElement(false)}>Record</IonButton>
+              </IonCol>
+            </IonRow>
+            <IonRow className="inputchoices">
+              <IonCol>
+                {showElement && <IonTextarea id="item" value={content} placeholder="Leave your message here" onIonChange={e => setContent(e.detail.value!)}></IonTextarea>}
+
+                {!showElement && <IonIcon icon={micOutline} size="large"></IonIcon>}
+              </IonCol>
+  
             </IonRow>
             <IonRow>
-              {showElement && <IonTextarea id="item" value={content} placeholder="Leave your message here" onIonChange={e => setContent(e.detail.value!)}>
-              </IonTextarea>}
+              <IonCol size="9">
+                <h5>Add image</h5>
+              </IonCol>
+              <IonCol size="3">
+                <IonButton color="secondary" onClick={takePicture}>
+                 <IonIcon slot="icon-only" icon={cameraOutline} size="large"></IonIcon>
+                </IonButton>
+              </IonCol>
             </IonRow>
             <IonRow>
-             {!showElement && <IonIcon icon={micOutline} size="large"></IonIcon>}
+              <IonCol size="9">
+                <h5>Enter with master key?</h5>
+              </IonCol>
+              <IonCol size="3">
+                <IonToggle color="tertiary" checked={masterkey} onIonChange={e => setMasterkey(e.detail.checked)}></IonToggle>
+              </IonCol>
             </IonRow>
             <IonRow>
-              Add image
-              <IonIcon icon={attachOutline} size="large"></IonIcon>
-              <IonIcon icon={cameraOutline} size="large"></IonIcon>
+              <IonCol size="9">
+                <h5>Call before entering?</h5>
+              </IonCol>
+              <IonCol size="3">
+                <IonToggle color="tertiary" checked={call} onIonChange={e => setCall(e.detail.checked)}></IonToggle>
+              </IonCol>
             </IonRow>
             <IonRow>
-              Enter with master key?
-              <IonToggle checked={masterkey} onIonChange={e => setMasterkey(e.detail.checked)}></IonToggle>
-            </IonRow>
-            <IonRow>
-              Call before entering?
-              <IonToggle checked={call} onIonChange={e => setCall(e.detail.checked)}></IonToggle>
-            </IonRow>
-            <IonRow>
-              <IonButton onClick={addReport }>
-              Send
-              </IonButton>
+              <IonCol>
+                <IonButton color="secondary" expand="block" onClick={addReport }>
+                  Send
+                </IonButton>
+              </IonCol>
             </IonRow>
           </IonGrid>         
         </IonModal>
-
-
       </IonContent>
-
+  
+      <IonFooter className="emergency">
+          <h6>Emergency contact details</h6>
+          <p>In case of emergency for ex. severely leaking water pipe or dangerous elecrical fault.<br/>
+          24/7</p>
+          <p id="phone">+358 123 456 789</p>
+      </IonFooter>
     </IonPage>
   );
 };
